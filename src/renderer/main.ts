9fb452523cb3e1384861afc753866a8882ce3f92
luizzrz <luizcazanatto200@gmail.com>
2024-06-20 16:43:59 -0300

    Estilização de template de dashboard

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -1,11 +1,93 @@
 import { createApp } from 'vue'
-import './style.css';
-import './assets/tailwind.css'
 import App from './App.vue'
 import { createPinia } from 'pinia'
+import { createWebHistory, createRouter } from "vue-router";
 
-const pinia = createPinia()
-const app = createApp(App);
+// styles
+// import './style.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./assets/styles/tailwind.css";
 
-app.use(pinia)
-app.mount('#app');
+// layouts
+
+import Admin from "./layouts/Admin.vue";
+// import Auth from "./layouts/Auth.vue";
+
+// views for Admin layout
+
+import Dashboard from "./views/admin/Dashboard.vue";
+import Settings from "./views/admin/Settings.vue";
+import Tables from "./views/admin/Tables.vue";
+import Maps from "./views/admin/Maps.vue";
+
+// views for Auth layout
+
+// import Login from "./views/auth/Login.vue";
+// import Register from "./views/auth/Register.vue";
+
+// views without layouts
+// import Landing from "./views/Landing.vue";
+// import Profile from "./views/Profile.vue";
+// import Index from "./views/Index.vue";
+
+// routes
+const routes = [
+  {
+    path: "/admin",
+    redirect: "/admin/dashboard",
+    component: Admin,
+    children: [
+      {
+        path: "/admin/dashboard",
+        component: Dashboard,
+      },
+      {
+        path: "/admin/settings",
+        component: Settings,
+      },
+      {
+        path: "/admin/tables",
+        component: Tables,
+      },
+      {
+        path: "/admin/maps",
+        component: Maps,
+      },
+    ],
+  },
+  // {
+  //   path: "/auth",
+  //   redirect: "/auth/login",
+  //   component: Auth,
+  //   children: [
+  //     {
+  //       path: "/auth/login",
+  //       component: Login,
+  //     },
+  //     {
+  //       path: "/auth/register",
+  //       component: Register,
+  //     },
+  //   ],
+  // },
+  // {
+  //   path: "/landing",
+  //   component: Landing,
+  // },
+  // {
+  //   path: "/profile",
+  //   component: Profile,
+  // },
+  // {
+  //   path: "/",
+  //   component: Index,
+  // },
+  { path: "/:pathMatch(.*)*", redirect: "/admin/dashboard" },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+createApp(App).use(router).mount("#app");
